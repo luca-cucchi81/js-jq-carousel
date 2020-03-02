6c311a15be548c9bb2f66471d45af22bf8ce5193
Luca <lu.cucchi@gmail.com>
2020-03-02 16:21:49 +0100

    fix slider

--- js/script.js
+++ js/script.js
@@ -6,12 +6,22 @@ function prevSlide() {
     if ( $('.images img.active').hasClass('first') ) {
         $('.images img.active').removeClass('active');
         $('.images img.last').addClass('active');
+
+        //pulsanti Slider
+        $('.button i.active').removeClass('active');
+        $('.button i.last').addClass('active');
+
     } else {
         var imgAttiva = $('.images img.active');
         var prevImg = $('.images img.active').prev();
-
         imgAttiva.removeClass('active');
         prevImg.addClass('active');
+
+        //pulsanti Slider
+        var pulsanteActive = $('.button i.active');
+        var pulsantePrecedente = $('.button i.active').prev();
+        pulsanteActive.removeClass('active');
+        pulsantePrecedente.addClass('active');
     }
 }
 
@@ -20,11 +30,21 @@ function nextSlide() {
     if ( $('.images img.active').hasClass('last') ) {
         $('.images img.active').removeClass('active');
         $('.images img.first').addClass('active');
+
+        //pulsanti Slider
+        $('.button i.active').removeClass('active');
+        $('.button i.first').addClass('active');
     } else {
         var imgAttiva = $('.images img.active');
         var nextImg = $('.images img.active').next();
-
         imgAttiva.removeClass('active');
         nextImg.addClass('active');
+
+        //pulsanti Slider
+        var pulsanteActive = $('.button i.active');
+        var pulsanteSuccessivo = $('.button i.active').next();
+        pulsanteActive.removeClass('active');
+        pulsanteSuccessivo.addClass('active');
+        console.log(pulsanteSuccessivo);
     }
 }
